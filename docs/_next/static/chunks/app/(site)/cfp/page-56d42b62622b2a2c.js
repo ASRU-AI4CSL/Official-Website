a73093ca9651d12997a20e5dd7a43ef4d901c36e
XulinFan <xulinf2@illinois.edu>
2025-08-16 15:56:15 -0500

    fix style issue

---
+++ docs/_next/static/chunks/app/(site)/cfp/page-56d42b62622b2a2c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[430,846,354,778,288,555,931],{3521:function(){}},function(n){n.O(0,[971,23,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
